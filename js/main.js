e39f9db2cbc5b62f043ee0c265ebb15925ad3575
Shrinidhi Sridhar <shrinidhisridhar30@gmail.com>
2018-03-05 22:47:35 -0500

    Fix the indentation issues

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var newName = prompt('Enter a letter');
 
 for (i = 0; i < 10; i++) {
-  for (j = 0; j < i; j++ ) {
+  for (j = 0; j <= i; j++) {
     document.write(newName);
   }
   document.write('<br>');
